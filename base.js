a64a204c287490c11adac6643e62947670d758fa
Jason King <jk@radical.gd>
2024-03-21 14:53:59 -0500

    nbsp

--- base.js
+++ base.js
@@ -18,9 +18,9 @@ const ws = /[ \t\n]+/
 const skills = {
 	good: "ASP AWS C++ C CVS Hotwire Chef Cucumber Debian Delphi Ferret IRC MSSQL Mason MongoDB OLE POP3 Puppet RMI SOAP Scala VBA WINAPI WSDL XP backbone.js go/golang merb mod_perl redis Mongrel NNTP Oracle Sybase lighttpd React".split(ws),
 
-	excellent: "Bulma UnoCSS GIS Elixir GraphQL IIS IMAP Jade Java PHP Passenger PostgreSQL REST SMTP SQL SQLite3 Scrum Sphinx Stylus TailwindCSS Ubuntu Visual Basic awk bash mSQL mod_rails node.js sed".split(ws),
+	excellent: "Bulma UnoCSS GIS Elixir GraphQL IIS IMAP Jade Java PHP Passenger PostgreSQL REST SMTP SQL SQLite3 Scrum Sphinx Stylus TailwindCSS Ubuntu Visual&nbsp;Basic awk bash mSQL mod_rails node.js sed".split(ws),
 
-	expert: `Slim AJAX Agile Batman.js CGI CSS Capistrano CoffeeScript CouchDB HTML HTTP JSON JavaScript Linux MySQL OSX Perl Phoenix xUnit Test::Unit Ruby on Rails Windows XHTML XML Ubuntu gentoo git haml heroku htmx jQuery nginx Apache prototype regex rspec scss/sass Vue`.split(ws),
+	expert: `Slim AJAX Agile Batman.js CGI CSS Capistrano CoffeeScript CouchDB HTML HTTP JSON JavaScript Linux MySQL OSX Perl Phoenix xUnit Test::Unit Ruby&nbsp;on&nbsp;Rails Windows XHTML XML Ubuntu gentoo git haml heroku htmx jQuery nginx Apache prototype regex rspec scss/sass Vue`.split(ws),
 }
 
 const skills_map = Object.entries(skills).reduce((h, [level, skills]) => {
